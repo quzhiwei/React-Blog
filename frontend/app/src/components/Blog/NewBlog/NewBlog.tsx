5ad849df710a970365d991e8623481dc8f676cdc
unknown <zhiweiqu@buffalo.edu>
2022-05-12 03:16:39 -0400

    added component for new blog components, linked up some of the functionality, added sample blog data

--- frontend/app/src/components/Blog/NewBlog/NewBlog.tsx
+++ frontend/app/src/components/Blog/NewBlog/NewBlog.tsx
@@ -1,32 +1,49 @@
-import React, { Component } from 'react'
-import { Input } from 'antd'
-
+import React, { Component } from "react";
+import { Input, Button, Avatar} from "antd";
+import { PlusCircleFilled ,UserOutlined } from "@ant-design/icons";
 
 export default class NewBlog extends Component {
-    state = {
-        value:'',
-    }
-    onChange = (e:any) => {
-        console.log('Change:', e.target.value);
-      };
+  state = {
+    blogContent: "",
+  };
+  onChange = (e: any) => {
+    console.log("Change:", e.target.value);
+    this.setState({ blogContent:e.target.value });
+  };
+  
+  submit = ()=>{
+    console.log(this.state.blogContent)
+    this.setState({ blogContent:'' });
+  }
   render() {
-    const {value} = this.state;
+    const { blogContent } = this.state;
 
-    const {TextArea} = Input;
+    const { TextArea } = Input;
     return (
       <div>
-
-        <TextArea
-        value={value}
-        showCount = {true} 
-        maxLength={100} 
-        style={{ height: 120}}
-        placeholder="Autosize height with minimum and maximum number of lines"
-        // autoSize={{ minRows: 2, maxRows: 6 }}
-        onChange = {this.onChange}
-        size = {'large'}
-        />
+        <div>
+          <Avatar size="large" icon={<UserOutlined />} />
+          <TextArea
+            value={blogContent}
+            showCount={true}
+            maxLength={100}
+            style={{ width: 500, margin: "auto" }}
+            placeholder="Say something..."
+            autoSize={{ minRows: 2, maxRows: 6 }}
+            onChange={this.onChange}
+            size={"small"}
+          />
+          <br />
+        </div>
+        <div id="newBlogUtil">
+          <div id="options">
+            <PlusCircleFilled />
+          </div>
+          <div id="submit">
+            <Button style={{ margin: "auto" }} onClick={this.submit} >Submit</Button>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
--- frontend/app/src/containers/BlogContainer/BlogContainer.tsx
+++ frontend/app/src/containers/BlogContainer/BlogContainer.tsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import Blog from '../../components/Blog/Blog/Blog'
 import NewBlog from '../../components/Blog/NewBlog/NewBlog'
+
+type BlogData = {
+  user:string,
+  content:string,
+}
+
 export default class BlogContainer extends Component {
+  state={
+
+  }
   render() {
+    const sampleData:BlogData[] = [{
+      user:'Jas',
+      content:'hello my name is Jas '
+    }]
     return (
       <div>
           <NewBlog />
