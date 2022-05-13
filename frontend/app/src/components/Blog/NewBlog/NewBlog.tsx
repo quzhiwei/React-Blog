5d24d7a7107eb0ffaeeca4af7baa9f58a3963377
unknown <zhiweiqu@buffalo.edu>
2022-05-13 01:12:33 -0400

    added tag feature to a new blog

--- frontend/app/src/components/Blog/NewBlog/NewBlog.tsx
+++ frontend/app/src/components/Blog/NewBlog/NewBlog.tsx
@@ -1,23 +1,55 @@
 import React, { Component } from "react";
-import { Input, Button, Avatar} from "antd";
-import { PlusCircleFilled ,UserOutlined } from "@ant-design/icons";
+import { Input, Button, Avatar, Dropdown, Menu, Space } from "antd";
+import {
+  PlusCircleFilled,
+  UserOutlined,
+  DownOutlined,
+} from "@ant-design/icons";
 
 export default class NewBlog extends Component {
   state = {
     blogContent: "",
+    tag: 'Mood',
+    items: [
+      {
+        label: "Daily",
+        key: "1",
+      },
+      {
+        label: "Gaming",
+        key: "2",
+      },
+      {
+        label: "Music",
+        key: "3",
+      },
+      {
+        label: "Others",
+        key: "4",
+      },
+    ]
   };
   onChange = (e: any) => {
     console.log("Change:", e.target.value);
-    this.setState({ blogContent:e.target.value });
+    this.setState({ blogContent: e.target.value });
   };
-  
-  submit = ()=>{
-    console.log(this.state.blogContent)
-    this.setState({ blogContent:'' });
+
+  submit = () => {
+    console.log(this.state.blogContent);
+    this.setState({ blogContent: "" });
+  };
+  updateTag = (e:any)=>{
+    console.log(this.state.items[e.key].label);
+    this.setState({tag:this.state.items[e.keys].label})
   }
   render() {
-    const { blogContent } = this.state;
-
+    const { blogContent, tag ,items} = this.state;
+    const menu = (
+      <Menu
+      onClick={this.updateTag}
+      items={items}
+      />
+    );
     const { TextArea } = Input;
     return (
       <div>
@@ -36,11 +68,23 @@ export default class NewBlog extends Component {
           <br />
         </div>
         <div id="newBlogUtil">
-          <div id="options">
+          <div id="options"  >
+            <Dropdown
+              overlay={menu}
+            >
+              <a onClick={(e) => e.preventDefault()}>
+                <Space>
+                  {tag}
+                  <DownOutlined />
+                </Space>
+              </a>
+            </Dropdown>
             <PlusCircleFilled />
           </div>
           <div id="submit">
-            <Button style={{ margin: "auto" }} onClick={this.submit} >Submit</Button>
+            <Button style={{ margin: "auto" }} onClick={this.submit}>
+              Submit
+            </Button>
           </div>
         </div>
       </div>
