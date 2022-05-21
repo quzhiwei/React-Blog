edf87c2e31076a1d19c7e53464edac4cf0c39e1b
unknown <zhiweiqu@buffalo.edu>
2022-05-20 22:17:51 -0400

    user able to click on msg icon to bring up textinput to make a comment

--- frontend/app/src/components/Blog/Blog/Blog.tsx
+++ frontend/app/src/components/Blog/Blog/Blog.tsx
@@ -1,24 +1,69 @@
-import React, { Component } from "react";
+import React, { ChangeEvent, Component } from "react";
 import style from "./index.module.scss";
-import { CommentOutlined } from "@ant-design/icons";
+import {
+  CommentOutlined,
+  CloseOutlined,
+  CheckOutlined,
+} from "@ant-design/icons";
+import { Input } from "antd";
 export default class Blog extends Component<any> {
+  state = {
+    canComment: false,
+    comment: "",
+  };
+
+  makeComment = () => {
+    this.setState({ canComment: true });
+  };
+
+  submitComment = () => {
+    this.setState({ canComment: false, comment: "" });
+  };
+
+  cancelComment = () => {
+    this.setState({ canComment: false });
+  };
+
+  updateComment = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ comment: e.target.value });
+  };
   render() {
+    const { canComment, comment } = this.state;
     const { user, content } = this.props.blog;
     const comments: string[] = this.props.blog.comments || [];
-    const { blog, blogOption, commentButton, viewComment } = style;
+    const { blog, blogOption, pointerCursor, viewComment } = style;
     return (
       <div className={blog}>
         {user}
         <br />
         {content}
         <div className={blogOption}>
-          <div className={commentButton}>
-            <CommentOutlined style={{ fontSize: "24px", color: "#08c" }} />
+          <div className={pointerCursor}>
+            <CommentOutlined
+              onClick={this.makeComment}
+              className={pointerCursor}
+              style={{ fontSize: "24px", color: "#08c" }}
+            />
           </div>
         </div>
+        {canComment && (
+          <div>
+            <Input size="small" value={comment} onChange={this.updateComment} />
+            <CloseOutlined
+              onClick={this.cancelComment}
+              className={pointerCursor}
+              style={{ fontSize: "18px", color: "#08c" }}
+            />
+            <CheckOutlined
+              onClick={this.submitComment}
+              className={pointerCursor}
+              style={{ fontSize: "18px", color: "#08c" }}
+            />
+          </div>
+        )}
         <div className={viewComment}>
-          {comments.map((comment: string) => {
-            return <div>{comment}</div>;
+          {comments.map((comment: string, index: number) => {
+            return <div key={index}>{comment}</div>;
           })}
         </div>
       </div>
